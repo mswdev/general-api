7a2db16578e092818a0a028554d57a821b0bd1cf
Sphiinx <sphiinx33@gmail.com>
2017-06-16 02:39:33 -0700

    Changes to Images.java

--- src/org/util/Images.java
+++ src/org/util/Images.java
@@ -2,6 +2,7 @@ package org.util;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -16,13 +17,31 @@ public class Images {
      * @param url The url of the image.
      * @return The image.
      */
-    public static Image getImage(String url) {
+    public static Image getImageFromURL(String url) {
         try {
             return ImageIO.read(new URL(url));
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
+
+        return null;
+    }
+
+
+    /**
+     * Gets the image from the path.
+     *
+     * @param path The path of the image.
+     * @return The image.
+     */
+    public static Image getImageFromPath(String path) {
+        try {
+            return ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }

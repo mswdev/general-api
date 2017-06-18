ed78b6acf72f29795b4afd4414dd5bfe98544428
Sphiinx <sphiinx33@gmail.com>
2017-06-17 18:22:09 -0700

    Added SQLDatabase.java

--- src/org/mysql/SQLDatabase.java
+++ src/org/mysql/SQLDatabase.java
@@ -1,8 +1,6 @@
 package org.mysql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created by Sphiinx on 6/17/2017.
@@ -19,6 +17,21 @@ public class SQLDatabase {
      */
     public static Connection getDatabase(String database_url, String database_username, String database_password) throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://" + database_url, database_username, database_password);
+    }
+
+    /**
+     * Queries the database table for the specified column.
+     *
+     * @param database_connection The database connection.
+     * @param database_table The database table.
+     * @param database_column The database column.
+     *
+     * @return The ResultSet from the query.
+     * */
+    public static ResultSet queryDatabase(Connection database_connection, String database_table, String database_column) throws SQLException {
+        final Statement STATEMENT = database_connection.createStatement();
+        final String QUERY = "SELECT " + database_table + " FROM " + database_column;
+        return STATEMENT.executeQuery(QUERY);
     }
 
 }

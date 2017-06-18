0b084a2a039cc0294810314945982c422f8c6d49
Sphiinx <sphiinx33@gmail.com>
2017-06-17 18:40:43 -0700

    Additions to SQLDatabase.java

--- src/org/mysql/SQLDatabase.java
+++ src/org/mysql/SQLDatabase.java
@@ -13,12 +13,13 @@ public class SQLDatabase {
      * @param database_connection The database connection.
      * @param database_table The database table.
      * @param database_column The database column.
+     * @param limit The limit to query for.
      *
      * @return The ResultSet from the query.
      * */
-    public static ResultSet queryDatabase(Connection database_connection, String database_table, String database_column) throws SQLException {
+    public static ResultSet queryDatabase(Connection database_connection, String database_table, String database_column, int limit) throws SQLException {
         final Statement STATEMENT = database_connection.createStatement();
-        final String QUERY = "SELECT " + database_table + " FROM " + database_column;
+        final String QUERY = "SELECT * FROM " + database_table + " WHERE username = '" + database_column + "' LIMIT " + limit;
         return STATEMENT.executeQuery(QUERY);
     }
 
